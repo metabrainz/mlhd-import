e52794e99c30ce5b18a2dcce09d914791976a2a5
Robert Kaye <rob@musicbrainz.org>
2017-11-11 17:56:09 +0000

    Broke job into threads

--- submit-to-bigquery.py
+++ submit-to-bigquery.py
@@ -7,6 +7,7 @@ import tarfile
 import gzip
 import io
 from time import sleep, time
+from threading import Thread, Lock
 
 import click
 from googleapiclient import discovery
@@ -29,89 +30,140 @@ class NoCredentialsFileException(Exception):
 class NoCredentialsVariableException(Exception):
     pass
 
-def handle_file(member, contents, bq_data):
-    comp = io.BytesIO()
-    comp.write(contents)
-    comp.seek(0)
+class FileImporter(Thread):
 
-    user = member.split(".")[0]
-    with gzip.GzipFile(fileobj=comp, mode='rb') as f:
-        for line in f.readlines():
-            line = line.strip()
-            cols = line.decode('ascii').split('\t')
-            if len(cols) == 4 and cols[3]:
-                data = {
-                    'listened_at' : cols[0],
-                    'user_name' : user,
-                    'artist_mbid' : cols[1],
-                    'release_mbid' : cols[2],
-                    'recording_mbid' : cols[3]
-                }
-                bq_data.append({
-                    'json': data,
-                    'insertId': "%s-%s-%s" % (data['user_name'], data['listened_at'], data['recording_mbid'])
-                })
+    def __init__(self, dir, file_index):
+        super(FileImporter, self).__init__()
+        self.done = False
+        self.dir = dir
+        self.file_index = file_index
 
-def submit(bigquery, bq_data):
-    body = { 'rows' : bq_data }
-    while True:
+    def is_done(self):
+        return self.done
+
+    def handle_file(self, member, contents, bq_data):
+        comp = io.BytesIO()
+        comp.write(contents)
+        comp.seek(0)
+
+        user = member.split(".")[0]
+        with gzip.GzipFile(fileobj=comp, mode='rb') as f:
+            for line in f.readlines():
+                line = line.strip()
+                cols = line.decode('ascii').split('\t')
+                if len(cols) == 4 and cols[3]:
+                    data = {
+                        'listened_at' : cols[0],
+                        'user_name' : user,
+                        'artist_mbid' : cols[1],
+                        'release_mbid' : cols[2],
+                        'recording_mbid' : cols[3]
+                    }
+                    bq_data.append({
+                        'json': data,
+                        'insertId': "%s-%s-%s" % (data['user_name'], data['listened_at'], data['recording_mbid'])
+                    })
+
+    def submit(self, bigquery, bq_data):
+        body = { 'rows' : bq_data }
+        while True:
+            try:
+                ret = bigquery.tabledata().insertAll(
+                    projectId=BIGQUERY_PROJECT_ID,
+                    datasetId=BIGQUERY_DATASET_ID,
+                    tableId=BIGQUERY_TABLE_ID,
+                    body=body).execute(num_retries=5)
+                return len(bq_data)
+
+            except HttpError as e:
+                print("Submit to BigQuery failed: %s. Retrying in 3 seconds." % str(e))
+
+            except Exception as e:
+                print("Unknown exception on submit to BigQuery failed: %s. Retrying in 3 seconds." % str(e))
+
+            sleep(ERROR_RETRY_DELAY)
+
+
+    def run(self):
+
+        if not os.path.exists(APP_CREDENTIALS_FILE):
+            logger.error("The BigQuery credentials file does not exist, cannot connect to BigQuery")
+            raise NoCredentialsFileException
+
         try:
-            ret = bigquery.tabledata().insertAll(
-                projectId=BIGQUERY_PROJECT_ID,
-                datasetId=BIGQUERY_DATASET_ID,
-                tableId=BIGQUERY_TABLE_ID,
-                body=body).execute(num_retries=5)
-            return len(bq_data)
+            credentials = GoogleCredentials.get_application_default()
+            bigquery = discovery.build('bigquery', 'v2', credentials=credentials)
+        except (NoCredentialsFileException, NoCredentialsVariableException):
+            self.log.error("Credential File not present or invalid! Sleeping...")
+            sleep(1000)
 
-        except HttpError as e:
-            print("Submit to BigQuery failed: %s. Retrying in 3 seconds." % str(e))
-
-        except Exception as e:
-            print("Unknown exception on submit to BigQuery failed: %s. Retrying in 3 seconds." % str(e))
-
-        sleep(ERROR_RETRY_DELAY)
-
-
-@click.command()
-@click.argument('dir')
-def import_tars(dir):
-
-    total = 0
-    last_update_count = 0
-    last_update = time()
-
-    if not os.path.exists(APP_CREDENTIALS_FILE):
-        logger.error("The BigQuery credentials file does not exist, cannot connect to BigQuery")
-        raise NoCredentialsFileException
-
-    try:
-        credentials = GoogleCredentials.get_application_default()
-        bigquery = discovery.build('bigquery', 'v2', credentials=credentials)
-    except (NoCredentialsFileException, NoCredentialsVariableException):
-        self.log.error("Credential File not present or invalid! Sleeping...")
-        sleep(1000)
-
-    bq_data = []
-    for index in range(NUM_FILES):
-        path = os.path.join(dir, "MLHD_%03d.tar" % index)
+        bq_data = []
+        path = os.path.join(self.dir, "MLHD_%03d.tar" % self.file_index)
         tar = tarfile.open(path)
         for member in tar.getnames():
-            handle_file(member, tar.extractfile(member).read(), bq_data)
-
+            self.handle_file(member, tar.extractfile(member).read(), bq_data)
             while len(bq_data) > SUBMIT_THRESHOLD:
-                count = submit(bigquery, bq_data[:SUBMIT_THRESHOLD])
-                total += count
-                last_update_count += count
+                count = self.submit(bigquery, bq_data[:SUBMIT_THRESHOLD])
+                update_total(count)
                 bq_data = bq_data[SUBMIT_THRESHOLD:]
-
-            if time() - last_update > 10.0:
-                print("file %d of %d: total %s, %s row/sec, %.2f%% complete" % (index, NUM_FILES, "{:,}".format(total), "{:,}".format(int(last_update_count / (time() - last_update))), total / TOTAL_COUNT))
-                last_update_count = 0
-                last_update = time()
 
         tar.close()
 
-    submit(bigquery, bq_data)
+        submit(bigquery, bq_data)
+        self.is_done = True
+
+
+NUM_THREADS = 10
+total = 0
+lock = Lock()
+
+def update_total(count):
+    global total
+    global lock
+    lock.acquire()
+    total += count
+    lock.release()
+
+def get_total():
+    global total
+    global lock
+    lock.acquire()
+    t = total
+    lock.release()
+    return t
  
+@click.command()
+@click.argument('dir')
+def import_data(dir):
+    global total
+
+    threads = []
+    last_update_total = 0
+    last_update = time()
+    for file_index in range(NUM_FILES):
+        if len(threads) < NUM_THREADS:
+            print("Start thread for file %d" % file_index)
+            thread = FileImporter(dir, file_index)
+            thread.start()
+            threads.append(thread)
+            continue
+
+        thread_ended = False
+        while not thread_ended:
+            for i, thread in enumerate(threads):
+                if thread.is_done():
+                    threads[i].join()
+                    del threads[i]
+                    thread_ended = True
+                    break
+
+            if time() - last_update > 10.0:
+                total = get_total()
+                print("total %s, %s row/sec, %.2f%% complete" % ("{:,}".format(total), "{:,}".format(int((total - last_update_total) / (time() - last_update))), total / TOTAL_COUNT))
+                last_update = time()
+                last_update_total = total
+
+            sleep(1)
+
 if __name__ == '__main__':
-    import_tars()
+    import_data()
